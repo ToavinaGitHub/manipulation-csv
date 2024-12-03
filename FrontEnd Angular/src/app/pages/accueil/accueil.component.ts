ef545cb420fce3413145308028fa7af04328d394
Zak <118508607+ToavinaGitHub@users.noreply.github.com>
2024-12-03 09:50:32 +0300

    Re-init

--- FrontEnd Angular/src/app/pages/accueil/accueil.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FormComponent } from '../../components/form/form.component';
-
-@Component({
-  selector: 'my-accueil',
-  templateUrl: './accueil.component.html',
-})
-export class AccueilComponent extends FormComponent{
-}
